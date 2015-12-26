0c808cdc44a41db431c8503b35730cfa579309f3
Allisson Azevedo <allisson@gmail.com>
2015-12-25 23:56:37 -0300

    Adicionando os detalhes para os endpoints.

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-router.use(function (req, res, next) {
+router.use(function(req, res, next) {
   var page = 1;
   if (req.query.page) {
     page = req.query.page;
   }
-  var limit = 20
+  var limit = 30
   var offset = (page - 1) * limit;
   req.limit = limit;
   req.offset = offset;
@@ -15,7 +15,7 @@ router.use(function (req, res, next) {
 });
 
 router.get('/parlamentares', function(req, res, next) {
-  // filters
+
   var where = {}
   if (req.query.ideCadastro) {
     where.ideCadastro = req.query.ideCadastro
@@ -37,8 +37,7 @@ router.get('/parlamentares', function(req, res, next) {
   if (req.query.sgPartido) {
     where.sgPartido = req.query.sgPartido
   }
-  console.log(req.offset);
-  console.log(req.limit);
+
   models.Parlamentar
     .findAndCountAll({
       where: where,
@@ -61,8 +60,26 @@ router.get('/parlamentares', function(req, res, next) {
 
 });
 
+router.get('/parlamentares/:id', function(req, res, next) {
+
+  models.Parlamentar.findById(req.params.id)
+    .then(function(object) {
+      if (object) {
+        res.json(object);
+      } else {
+        res.status(404).json({
+          error: 'Recurso não encontrado'
+        });
+      }
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+
+});
+
 router.get('/subcotas', function(req, res, next) {
-  // filters
+
   var where = {}
   if (req.query.numSubCota) {
     where.numSubCota = req.query.numSubCota
@@ -76,9 +93,9 @@ router.get('/subcotas', function(req, res, next) {
   models.SubCota
     .findAndCountAll({
       where: where,
-      include: [
-        {model: models.EspecificacaoSubCota}, 
-      ],
+      include: [{
+        model: models.EspecificacaoSubCota
+      }, ],
       offset: req.offset,
       limit: req.limit,
       order: [
@@ -98,8 +115,34 @@ router.get('/subcotas', function(req, res, next) {
 
 });
 
+router.get('/subcotas/:id', function(req, res, next) {
+
+  models.SubCota
+    .findOne({
+      where: {
+        numSubCota: req.params.id
+      },
+      include: [{
+        model: models.EspecificacaoSubCota
+      }, ],
+    })
+    .then(function(object) {
+      if (object) {
+        res.json(object);
+      } else {
+        res.status(404).json({
+          error: 'Recurso não encontrado'
+        });
+      }
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+
+});
+
 router.get('/despesas', function(req, res, next) {
-  // filters
+
   var where = {}
   if (req.query.txtFornecedor) {
     where.txtFornecedor = {
@@ -137,6 +180,24 @@ router.get('/despesas', function(req, res, next) {
         pages: Math.ceil(result.count / req.limit),
         data: result.rows
       })
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+
+});
+
+router.get('/despesas/:id', function(req, res, next) {
+
+  models.Despesa.findById(req.params.id)
+    .then(function(object) {
+      if (object) {
+        res.json(object);
+      } else {
+        res.status(404).json({
+          error: 'Recurso não encontrado'
+        });
+      }
     })
     .catch(function(err) {
       res.send(err);
